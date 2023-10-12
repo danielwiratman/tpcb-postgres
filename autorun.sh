8cf9828c945a281e49ac2ec6faf4456a21e2a2fe
Daniel Wiratman <danielwiratman@gmail.com>
2023-10-12 11:59:58 +0700

    fix autorun

--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,6 @@
 NUM_THREAD=12
-NUM_SEC=30
+NUM_SEC=15
 
 ./test_postgres -i -s $NUM_THREAD
 
-./test_postgres -s $NUM_THREAD -c $NUM_THREAD -T $NUM_THREAD
+./test_postgres -s $NUM_THREAD -c $NUM_THREAD -T $NUM_SEC
