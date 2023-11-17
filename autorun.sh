9df5cfc1f320a9e8cf690faa81f546793c7e8c58
Daniel Wiratman <danielwiratman@gmail.com>
2023-11-17 09:17:04 +0700

    fix autorun script

--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,10 @@
-NUM_THREAD=12
-NUM_SEC=15
+NUM_THREAD=12 # Bisa cek di htop atau proc, ada berapa thread
+NUM_SEC=15 # Accepted value for true benchmark is usually 300-600
 
+make
+
+# Initializing tables
 ./test_postgres -i -s $NUM_THREAD
 
+# Doing the test
 ./test_postgres -s $NUM_THREAD -c $NUM_THREAD -T $NUM_SEC
