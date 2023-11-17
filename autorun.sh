63d971ca673efa74ad294edf0f464d87de284ce9
Daniel Wiratman <danielwiratman@gmail.com>
2023-11-17 09:13:49 +0700

    added make to autorun sh

--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,8 @@
 NUM_THREAD=12 # Bisa cek di htop atau proc, ada berapa thread
 NUM_SEC=15 # Accepted value for true benchmark is usually 300-600
 
+make
+
 # Initializing tables
 ./test_postgres -i -s $NUM_THREAD
 
